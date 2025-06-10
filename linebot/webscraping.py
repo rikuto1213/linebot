7d1785aac0c6b745e39c9c5291f3ab1e84c66eb3
rikuto1213 <nisriku@gmail.com>
2025-06-10 16:20:08 +0900

    unioncafeのURL更新

--- linebot/webscraping.py
+++ linebot/webscraping.py
@@ -2,9 +2,9 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 driver=webdriver.Chrome()
-driver.get("https://engineer-education.com/double-hetero-junction/")#立命の食堂のURLでおけ
+driver.get("https://west2-univ.jp/sp/menu.php?t=650311")#立命の食堂のURLでおけ
 
 images = driver.find_elements(By.TAG_NAME, "img")
 
-#print(f"find{len(images)}pictures.")画像タグの発見
+#print(f"find{len(images)}pictures.")画像タグ
 
