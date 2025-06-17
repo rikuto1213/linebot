125bf77efd6c8784a86513af22ed4751f7b5b034
rikuto1213 <nisriku@gmail.com>
2025-06-17 15:38:09 +0900

    savepicいらんくなった。

--- bot_app/urls.py
+++ bot_app/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('post/edit/<int:post_id>/', views.edit_post, name='edit_post'),  # 修正
     path('post/', views.read_post, name='read_post'),   # 一覧表示
     path('post/delete/<int:post_id>/', views.delete_post, name='delete_post'),   # 削除
-    path('post/savepic/' ,views.savepic,name='savepic'),
+    
     path('post/postpic/',views.Pic_post,name='pic_post')
 ]
